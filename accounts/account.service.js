036273d76a7a3c8d3205e18e1fcaacfa8a065152
ogbon <onethinneedful@gmail.com>
2021-05-22 21:42:38 +0100

    add documentation

--- accounts/account.service.js
+++ accounts/account.service.js
@@ -2,9 +2,9 @@ const config = require('config.json');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const crypto = require("crypto");
-const sendEmail = require('../_helpers/send-email');
-const db = require('../_helpers/db');
-const Role = require('../_helpers/role');
+const sendEmail = require('_helpers/send-email');
+const db = require('_helpers/db');
+const Role = require('_helpers/role');
 
 module.exports = {
     authenticate,
